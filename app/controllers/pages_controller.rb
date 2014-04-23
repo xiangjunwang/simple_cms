f2ed46b063a281db3406fa9eda869a4f59b5d997
XiangJun Wang <xiangjunwang321@gmail.com>
2014-04-22 20:46:52 -0400

    subject controller updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+
+	layout 'admin'
+
+	def index
+		list
+		render('list')
+	end
+
+	def list
+		@pages = Page.order("pages.position ASC")
+	end
+
+	def show
+		@page = Page.find(params[:id])
+	end
 end
