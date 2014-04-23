b86a242479a8755adae34ab2d4545aadf91ad842
XiangJun Wang <xiangjunwang321@gmail.com>
2014-04-22 23:06:43 -0400

    pages controller updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,4 +14,52 @@ class PagesController < ApplicationController
 	def show
 		@page = Page.find(params[:id])
 	end
+
+	def new
+		@page = Page.new
+	end
+
+	def create
+		# Instantiate a new object using form parameters
+		@page = Page.new(params[:page])
+		# Save the object
+		if @page.save 
+			# If save succeeds, refirect to the list action
+			flash[:notice] = "Page created successfully."
+			redirect_to(:action => 'list')
+		else
+			# If save filas, redisplay the form so user can fix problems
+			render('new')
+		end
+	end
+
+	def edit
+		@page = Page.find(params[:id])
+	end
+
+	def update
+		# Find a new object using form parameters
+		@page = Page.find(params[:id])
+		# Update the object
+		if @page.update_attributes(params[:page]) 
+			# If save succeeds, refirect to the list action
+			flash[:notice] = "Page updated successfully."
+			redirect_to(:action => 'show', :id => @page.id)
+		else
+			# If save filas, redisplay the form so user can fix problems
+			render('edit')
+		end
+	end
+
+	def delete
+		# Find a new object using form parameters
+		@page = Page.find(params[:id])
+	end
+
+	def destroy
+		Page.find(params[:id]).destroy
+		flash[:notice] = "Page destroyed successfully."
+		redirect_to(:action => 'list')
+	end
+
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,2 +1,14 @@
 class SectionsController < ApplicationController
+
+	layout 'admin'
+
+	def index
+		list
+		render('list')
+	end
+
+	def list
+		@sections = Sections.order("sections.position ASC")
+	end
+	
 end
